fafefcd8660e2785ead4889e16ea0d760cd30785
Anh Ngo <ngotruonganh1901.com>
2023-06-26 15:00:38 +0700

    add loading component

--- src/components/loading/index.jsx
+++ src/components/loading/index.jsx
@@ -2,7 +2,7 @@ import {PacmanLoader} from "react-spinners";
 
 const Loading = () => {
     return (
-        <div>
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100%"}}>
             <PacmanLoader color="#36d7b7" />
         </div>
     );
