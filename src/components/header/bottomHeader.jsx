133e02ca09ac9411b7ceae5b0359a8ea9c3abfc5
Anh Ngo <ngotruonganh1901.com>
2023-07-05 12:03:20 +0700

    fix header logo

--- src/components/header/bottomHeader.jsx
+++ src/components/header/bottomHeader.jsx
@@ -5,11 +5,11 @@ const BottomHeader = () => {
     return (
         <Row justify="space-between" align="middle" style={{padding: "5px 0"}}>
             <Col span={24}>
-                <Link to="/">
-                    <Typography.Title level={3} style={{color: "white"}}>
-                        Shop
+                    <Typography.Title level={3}>
+                        <Link to="/" style={{color: "white"}}>
+                            Shop
+                        </Link>
                     </Typography.Title >
-                </Link>
             </Col>
         </Row>
     );
